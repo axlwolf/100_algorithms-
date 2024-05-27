a0d8331ca4fb9677cd224dd8e46365ab2b5c0a06
axlwolf <axlwolf@gmail.com>
2024-05-27 17:50:35 +0000

    3. Complete 1 challege and change jest environment to js rather than using ts

--- absoluteValuesSumMinization/absoluteValuesSumMinization.js
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.js
@@ -1,5 +1,9 @@
-export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+export const absoluteValuesSumMinimization = (a) => {
+    const isEven = a.length % 2 === 0;
+    const middle = Math.floor(a.length / 2);
+
+    console.log({isEven, middle});
+    return isEven ? a[middle - 1] : a[middle];
 }
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
